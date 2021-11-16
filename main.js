981f24234f6b87d6a3848a28dafc9dbc363e719b
Albaer33 <alisi3396@gmail.com>
2021-11-16 10:40:09 +0100

    levato il getelementbyID e usato il queryselector

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 // importo tasti su javascript
-const myHamburgher = document.getElementById('hamburgher');
-const hiddenMenu = document.getElementById('hidden-menu');
-const myCross = document.getElementById('cross');
-
+const myHamburgher = document.querySelector('.header-right > a');
 myHamburgher.addEventListener('click',
     function() {
         // al click sovrascrivo la classe active
+        const hiddenMenu = document.querySelector('.hamburger-menu');
+        console.log(hiddenMenu);
         hiddenMenu.classList.add('active');
     }
 );
+
+const myCross = document.querySelector('.close');
 myCross.addEventListener('click',
     function() {
         // chiudo il menù
+        const hiddenMenu = document.querySelector('.hamburger-menu');
         hiddenMenu.classList.remove('active');
     }
 );
